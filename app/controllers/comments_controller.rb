89d1e6a3baca12f7b5441b3440595a8555aeb154
unknown <grenson.crtis@gmail.com>
2020-04-07 11:31:33 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+	def create
+		@book = Book.find(params[:book_id])
+		@comment = @book.comments.build(comment_params)
+		@comment.user_id = current_user.id
+		@comment.save
+		render :index
+	end
+
+	def destroy
+		@comment = Comment.find(params[:book_id])
+		@comment.destroy
+		render :index
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:comment, :book_id, :user_id)
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root 'home#top'
+  get 'home/about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+  	resource :comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
+
+end
